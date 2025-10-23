b99885385fe98018ca4e82af9c72ddb24a9fe32a
Yash Singh <yashsingh93804@gamil.com>
2025-10-24 00:44:07 +0530

    Remove frontend, backend, and dashboard from github and keep it locally

--- backend/model/OrderModel.js
+++
@@ -1,7 +0,0 @@
-const {model} = require("mongoose");
-
-const {OrderSchema} = require('../schemas/OrderSchema');
-
-const OrderModel  = new model("orders", OrderSchema);
-
-module.exports = {OrderModel};
